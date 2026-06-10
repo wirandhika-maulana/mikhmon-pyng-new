29c89588b85921654a595825ca6322ce37909552
Support <support@melsa.co.id>
2026-06-11 05:12:31 +0700

    modified:   ppp/traffic.php

--- ppp/traffic.php
+++ ppp/traffic.php
@@ -17,8 +17,24 @@ if(!isset($_SESSION["mikhmon"])){
 	$API = new RouterosAPI();
 	$API->debug = false;
 
+    // Load Dual Router Config (Secondary PPPoE Router)
+    $dual_router_ip = "";
+    if (file_exists('../include/dual_router_config.php')) {
+        include('../include/dual_router_config.php');
+        if (isset($dual_router[$session]) && !empty($dual_router[$session]['ip'])) {
+            $dual_router_ip = $dual_router[$session]['ip'];
+            $dual_router_user = $dual_router[$session]['user'];
+            $dual_router_pass = decrypt($dual_router[$session]['pass']);
+        }
+    }
 
-	if($API->connect( $iphost, $userhost, decrypt($passwdhost))){
+    if (!empty($dual_router_ip)) {
+        $connected = $API->connect($dual_router_ip, $dual_router_user, $dual_router_pass);
+    } else {
+	    $connected = $API->connect($iphost, $userhost, decrypt($passwdhost));
+    }
+
+	if($connected){
 
 //		$dtgrafik = $API->comm("/queue/simple/print",['?name'=>"$idproses",]);
 		$dtgrafik = $API->comm("/interface/monitor-traffic", array(
